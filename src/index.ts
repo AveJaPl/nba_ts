30c498a7645d135b9a532a289a0922d2c69e7072
Piatekf <filip.piatek2005@gmail.com>
2023-10-25 22:12:53 +0200

    Revert "deleted images"
    This reverts commit 355cc978683d29f9e82dbe8174f01d03a1e4dedc.

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import { getPlayer } from './getPlayer';
 import { IPlayer } from '../interfaces/IPlayer';
 import { modifyPlayer } from './modifyPlayer';
 import {upload} from '../multer_config';
+import formidable from 'formidable';
 
 const app = express();
 const port = 3000;
@@ -38,6 +39,9 @@ app.post('/addPlayer', upload.single('playerImage'), async (req: Request, res: R
 
         if (!player.salary) {
             player.salary = 0;
+        }
+        if (req.file) {
+            player.imagePath = req.file.path;
         }
         await addPlayer(player);
     } catch (e) {
