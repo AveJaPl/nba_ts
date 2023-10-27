9400c2a2fd9934401d99271fc8949c802c528928
Piatekf <filip.piatek2005@gmail.com>
2023-10-27 20:41:55 +0200

    interfaces modified

---
+++ nba_ts/client/src/interfaces/Player.tsx
@@ -0,0 +1,11 @@
+interface Player{
+    id: number;
+    name: string;
+    surname: string;
+    position: string;
+    nationality: string;
+    team_id: number;
+    salary: number;
+}
+
+export default Player;
---
+++ nba_ts/client/src/interfaces/Team.tsx
@@ -0,0 +1,9 @@
+interface Team {
+    id: number;
+    name: string;
+    city: string;
+    conference: string;
+    division: string;
+}
+
+export default Team;
