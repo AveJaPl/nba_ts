704be53db2da2def778363268189d247d35aa56a
Piatekf <filip.piatek2005@gmail.com>
2023-10-27 20:41:05 +0200

    added more routes

--- nba_ts/backend/src/server.ts
+++ nba_ts/backend/src/server.ts
@@ -1,24 +1,27 @@
 import express from 'express';
-import { Request, Response } from 'express';
-import { getPlayers } from './services/getPlayers';
-import { getTeams } from './services/getTeams';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import AddPlayerRouter from './routes/addPlayerRoute';
 import ModifyPlayerRouter from './routes/modifyPlayerRoute';
 import DeletePlayerRouter from './routes/deletePlayerRoute';
 import getAllPlayersRouter from './routes/getAllPlayers';
+import getAllTeamsRouter from './routes/getAllTeams';
+import GetPlayerRouter from './routes/getPlayerRoute';
 const app = express();
+
+app.use(cors())
 const port = 3000;
-const path = require('path');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('json spaces', 2);
 
 app.use('/getAllPlayers', getAllPlayersRouter)
+app.use('/getAllTeams', getAllTeamsRouter)
 //  upload.single('playerImage'),
 app.use('/addPlayer', AddPlayerRouter );
 app.use('/deletePlayer/:id', DeletePlayerRouter);
-app.use('/editPlayer/:id', ModifyPlayerRouter);
+app.use('/editPlayer/', ModifyPlayerRouter);
+app.use('/getPlayer/', GetPlayerRouter);
 
 
 
