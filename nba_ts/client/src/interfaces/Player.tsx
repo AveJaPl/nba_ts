5365809441fbc0f6ac7a0e2a02dee342e489eab1
Piatekf <filip.piatek2005@gmail.com>
2023-10-27 22:35:30 +0200

    added reference to teams

--- nba_ts/client/src/interfaces/Player.tsx
+++ nba_ts/client/src/interfaces/Player.tsx
@@ -1,11 +1,15 @@
-interface Player{
+import Team from './Team';
+
+interface Player {
     id: number;
     name: string;
     surname: string;
-    position: string;
-    nationality: string;
-    team_id: number;
-    salary: number;
-}
+    team_id?: number;  // Jest opcjonalny w schemacie
+    nationality?: string; // Jest opcjonalny w schemacie
+    position?: string; // Jest opcjonalny w schemacie
+    salary: number; // Choć w bazie jest Decimal, w JS będzie to przetwarzane jako number
+    imagePath?: string;
+    team?: Team; // Referencja do interfejsu Team
+  }
 
-export default Player;
+  export default Player;
