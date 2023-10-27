f7a75b952f1c35c26d068ad7ddbd9ce372572488
Piatekf <filip.piatek2005@gmail.com>
2023-10-27 21:17:14 +0200

    fixed bug with tailwind

--- nba_ts/client/tailwind.config.js
+++ nba_ts/client/tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'nba-blue': '#1D428A',
+        'nba-red': '#CE1141',
+        'nba-gray': '#BCC4CC',
+        'nba-white': '#FFFFFF',
+
+    },
   },
   plugins: [],
 }
-
+}
