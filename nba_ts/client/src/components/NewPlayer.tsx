b769d7e2704ecf8663a2ca26bf6877bd57fc4845
Piatekf <filip.piatek2005@gmail.com>
2023-10-28 13:49:52 +0200

    cropping display

--- nba_ts/client/src/components/NewPlayer.tsx
+++ nba_ts/client/src/components/NewPlayer.tsx
@@ -1,13 +1,25 @@
-import { useState, ChangeEvent, useEffect } from "react";
+import React, { useState, ChangeEvent, useEffect } from "react";
 import Team from "../interfaces/Team";
 import axios from "axios";
-const NewPlayer = () => {
-  const [imagePreview, setImagePreview] = useState<string | null>(null);
-  const [teams, setTeams] = useState<Team[]>([]);
+import ReactCrop, { Crop } from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
 
-  const inputClass =
-    "hover:bg-nba-blue hover:text-white mt-1 w-full p-3 border rounded-md text-lg ";
-  const labelClass = "block text-nba-gray text-lg font-medium mb-2o";
+const NewPlayer: React.FC = () => {
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+
+  const [crop, setCrop] = useState<Crop>({
+    unit: "%",
+    x: 0,   // początkowa pozycja X
+    y: 0,   // początkowa pozycja Y
+    height: 155,
+    width: 115,
+    });
+  
+    
+  const [teams, setTeams] = useState<Team[]>([]);
+  
+  const inputClass = "hover:bg-nba-blue hover:text-white mt-1 w-full p-3 border rounded-md text-lg ";
+  const labelClass = "block text-nba-gray text-lg font-medium mb-2";
 
   const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files && e.target.files[0];
@@ -15,15 +27,50 @@ const NewPlayer = () => {
       const reader = new FileReader();
       reader.onloadend = () => {
         setImagePreview(reader.result as string);
+
+        const img = new Image();
+        img.src = reader.result as string;
+        img.onload = () => {
+          const targetAspect = 115 / 155;
+          const imageAspect = img.width / img.height;
+
+          if (imageAspect > targetAspect) {
+            // Obraz jest szerszy niż docelowy wymiar
+            const cropHeight = 100;
+            const cropWidth = (cropHeight * targetAspect) / imageAspect;
+            const cropX = (100 - cropWidth) / 2;
+
+            setCrop({
+              unit: "%",
+              x: cropX,
+              y: 0,
+              width: cropWidth,
+              height: cropHeight,
+            });
+          } else {
+            // Obraz jest węższy lub ma takie same proporcje jak docelowy wymiar
+            setCrop({
+              unit: "%",
+              x: 0,
+              y: 0,
+              width: 115,
+              height: 155,
+            });
+          }
+        };
       };
       reader.readAsDataURL(file);
     }
   };
 
+  const handleImageCrop = (crop: Crop) => {
+    setCrop(crop);
+  };
+
   useEffect(() => {
-    Promise.all([axios.post("http://localhost:3000/getAllTeams")])
-      .then(([teamsResponse]) => {
-        setTeams(teamsResponse.data);
+    axios.post("http://localhost:3000/getAllTeams")
+      .then((response) => {
+        setTeams(response.data);
       })
       .catch((err) => {
         console.log(err);
@@ -33,14 +80,16 @@ const NewPlayer = () => {
   return (
     <div className="bg-dark-primary h-screen w-full flex justify-center m-auto">
       <div className="container mx-8 w-full md:w-1/2 lg:w-2/5">
-      <div className="text-center mb-8 text-3xl font-bold text-nba-gray">
+        <div className="text-center mb-8 text-3xl font-bold text-nba-gray">
           Add New Player
         </div>
-        <form action="http://localhost:3000/addPlayer" method="post" encType="multipart/form-data">
+        <form
+          action="http://localhost:3000/addPlayer"
+          method="post"
+          encType="multipart/form-data"
+        >
           <div className="form-group">
-            <label className={labelClass}>
-              Name
-            </label>
+            <label className={labelClass}>Name</label>
             <input
               type="text"
               className={inputClass}
@@ -51,9 +100,7 @@ const NewPlayer = () => {
             />
           </div>
           <div className="form-group">
-            <label className={labelClass}>
-              Surname
-            </label>
+            <label className={labelClass}>Surname</label>
             <input
               type="text"
               className={inputClass}
@@ -64,9 +111,7 @@ const NewPlayer = () => {
             />
           </div>
           <div className="form-group">
-            <label className={labelClass}>
-              Position
-            </label>
+            <label className={labelClass}>Position</label>
             <select
               className={inputClass}
               id="position"
@@ -81,9 +126,7 @@ const NewPlayer = () => {
             </select>
           </div>
           <div className="form-group">
-            <label className={labelClass}>
-              Nationality
-            </label>
+            <label className={labelClass}>Nationality</label>
             <input
               type="text"
               className={inputClass}
@@ -94,9 +137,7 @@ const NewPlayer = () => {
             />
           </div>
           <div className="form-group">
-            <label className={labelClass}>
-              Team
-            </label>
+            <label className={labelClass}>Team</label>
             <select className={inputClass} id="team" name="team_id">
               {teams.map((team) => (
                 <option value={team.id} key={team.id}>
@@ -106,9 +147,7 @@ const NewPlayer = () => {
             </select>
           </div>
           <div className="form-group">
-            <label className={labelClass}>
-              Salary
-            </label>
+            <label className={labelClass}>Salary</label>
             <input
               type="number"
               className={inputClass}
@@ -128,19 +167,19 @@ const NewPlayer = () => {
               onChange={handleImageChange}
             />
           </div>
-          <div className="d-inline-block" style={{ width: "100%" }}>
-            <img
-              id="previewImage"
-              src={imagePreview || "#"}
-              alt="Podgląd wybranego zdjęcia"
-              style={{ width: "100%" }}
-            />
-          </div>
+          {imagePreview && (
+            <ReactCrop
+              crop={crop}
+              onChange={handleImageCrop}
+              aspect = {115/155 as number}
+              locked = {true}
+            ><img src={imagePreview} style={{height: '155px'}}/></ReactCrop>
+            
+          )}
 
           <button className="mt-4 w-full bg-nba-red hover:text-nba-gray text-white p-3 rounded text-lg transition duration-300">
-              Update Player
-            </button>
-
+            Add New Player
+          </button>
         </form>
       </div>
     </div>
